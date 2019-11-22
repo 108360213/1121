31f9023c13fda691e07c32a85a9750fdd279703a
108360213 <55587808+108360213@users.noreply.github.com>
2019-11-22 08:53:34 +0800

    Add files via upload

---
+++ P14/source/main.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define students 3
+#define exams 4
+
+void printarray(const int grades[][exams], int pupils, int tests)
+{
+	int i, j;
+	printf("               [0]  [1]  [2]  [3]");
+	for (i = 0; i < pupils; i++)
+	{
+		printf("\nstudentgrade[%d]", i);
+		for (j = 0; j < tests; j++)
+			printf("%-5d", grades[i][j]);
+	}
+}
+int minimum(const int grades[][exams], int pupils, int tests)
+{
+	int i, j;
+	int lowgrade = 100;
+	for (i = 0; i < pupils; i++)
+	{
+		for (j = 0; j < tests; j++)
+		{
+			if (grades[i][j] < lowgrade)
+				lowgrade = grades[i][j];
+		}
+	}
+	return lowgrade;
+}
+int maximum(const int grades[][exams], int pupils, int tests)
+{
+	int i, j;
+	int highgrade = 0;
+	for (i = 0; i < pupils; i++)
+	{
+		for (j = 0; j < tests; j++)
+		{
+			if (grades[i][j] > highgrade)
+				highgrade = grades[i][j];
+		}
+	}
+	return highgrade;
+}
+double average(const int setofgrades[], int tests)
+{
+	int i;
+	int total = 0;
+	for (i = 0; i < tests; i++)
+		total += setofgrades[i];
+	return  (double)total / tests;
+}
+int main(void)
+{
+	int student;
+	const int studentgrades[students][exams] = { {77,68,86,73},{96,87,89,78},{70,90,86,81} };
+	printf("The array is:\n");
+	printarray(studentgrades, students, exams);
+
+	printf("\n\nLowest grade: %d\nHighest grade: %d\n", minimum(studentgrades, students, exams), maximum(studentgrades, students, exams));
+	for (student = 0; student < students; student++)
+	{
+		printf("The average grade for student %d is %.2f\n", student, average(studentgrades[student], exams));
+	}
+	system("pause");
+	return 0;
+}
